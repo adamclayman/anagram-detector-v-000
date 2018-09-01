b11d83f9d1bcd149a56e9a82d0af065e376b7813
Adam Clayman <adam@clayman.org>
2018-09-01 16:56:52 +0000

    automatically backed up by learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -6,7 +6,7 @@ class Anagram
     self.word = word.downcase
   end
 
-  def self.match(words_array)
+  def match(words_array)
     words_string = words_array.join(",")
     words_string.downcase.scan(self.word.reverse)
   end
