e4bbe30cb2e2def69c709ac0307dd3321a5aa23c
Adam Clayman <adam@clayman.org>
2018-09-01 16:40:14 +0000

    automatically backed up by learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,16 +1,13 @@
 # Your code goes here!
 class Anagram
   attr_accessor :word
-  
+
   def initialize(word)
     self.word = word
   end
 
   def match(words_array)
-    words_array.each do |candidate|
-      if candidate == self.word.reverse
-        return candidate
-      end
+    words.match(self.word.reverse)
     end
   end
 end
