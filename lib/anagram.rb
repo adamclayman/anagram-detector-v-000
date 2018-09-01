bdf30c7c719281a11935a14f4808ce9582e769f3
Adam Clayman <adam@clayman.org>
2018-09-01 16:43:37 +0000

    automatically backed up by learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -7,10 +7,6 @@ class Anagram
   end
 
   def match(words_array)
-    words_array.each do |candidate|
-      if candidate.downcase == self.word.downcase.reverse
-        return candidate.to_a
-      end
-    end
+    words_array.match(self.word.downcase.reverse)
   end
 end
